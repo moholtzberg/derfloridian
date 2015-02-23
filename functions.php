e80571c44ca91fecab53caa850f5792060eb98d6
Moshe Holtzberg <moholtzberg@gmail.com>
2015-02-23 17:36:27 -0500

    update functions

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
 <?php
-update_option('siteurl','http://www.derfloridian.com');
-update_option('home','http://www.derfloridian.com');
+update_option('siteurl','http://derfloridian-wp.herokuapp.com');
+update_option('home','http://www.derfloridian-wp.herokuapp.com');
 ?>
